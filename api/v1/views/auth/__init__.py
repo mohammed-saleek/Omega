5d82444fdca47308a71bd4207d76d7fd2f640810
Saleek <mohammedsaleek786@gmail.com>
2023-01-18 22:34:54 +0530

    Added Sample params

--- api/v1/views/auth/__init__.py
+++ api/v1/views/auth/__init__.py
@@ -6,3 +6,16 @@ class TodoListApiView(APIView):
         data = {}
         data['message'] = "Testing"
         return Response(data)
+
+class test_params(APIView):
+    def get(self,request,*args,**kwargs):
+        # filter = self.kwargs['filter']
+        filter = request.GET.get("filter","")
+        test_item = request.GET.get("testitem","")
+        # test_item = self.kwargs['testitem']
+        # category = self.kwargs['category_id']
+        data = {}
+        data['test-item'] = test_item
+        data['filter'] = filter
+        data['message'] = "Testing"
+        return Response(data)
