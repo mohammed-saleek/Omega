1938af60b6a601c0c7be3171389c8488d9453458
Saleek <mohammedsaleek786@gmail.com>
2024-05-01 10:34:21 +0530

    added user license to all users

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from accounts.manager import CustomUserManager
 # Create your models here.
 
+class LicenceType(models.Model):
+    name = models.CharField(max_length=25)
+    admin = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name='email',max_length=50,unique=True)
     username = models.CharField(max_length=40,unique=True)
@@ -11,6 +20,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=False)
     token = models.CharField(max_length=255, null=True, blank=True)
+    licence_type = models.ForeignKey(LicenceType, on_delete=models.CASCADE, blank=True, null=True)
     
     objects = CustomUserManager()
     USERNAME_FIELD = 'email'
